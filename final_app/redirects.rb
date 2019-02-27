4f088737a65adfed2d760c5ee455262c5231e851
Paige <ecalvert@pivotal.io>
2019-02-27 13:30:42 -0700

    Final app and gitignore file [#164181414]

--- final_app/redirects.rb
+++ final_app/redirects.rb
@@ -5,4 +5,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirects for docs-book-healthwatch
 
-r301 %r{/pcf-healthwatch/(?![\d-]+)(.*)}, "/pcf-healthwatch/1-4/$1"
+r301 %r{/observability-data-store/(?![\d-]+)(.*)}, "/observability-data-store/1-0/$1"
